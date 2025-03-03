32d26baf16a21e52190aa078f9660ec93bdf67a1
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-03-03 16:45:33 +0100

    feat: add email configuration and health-check endpoint to Flask app

--- back/main.py
+++ back/main.py
@@ -1,21 +1,39 @@
+"""Main module"""
+import os
 from flask import jsonify
 import features.pdf as pdf
 
 from flask import Flask
+from flask_mail import Mail, Message
+from dotenv import load_dotenv
 
-def create_app():
-    """main function"""
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+load_dotenv()
 
-    @app.route('/hello')
-    def hello():
-        return pdf.generate_pdf()
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
+app.config['MAIL_SERVER']= os.environ.get('MAIL_SERVER')
+app.config['MAIL_PORT'] = os.environ.get('MAIL_PORT')
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PWD')
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+mail = Mail(app)
+
+@app.route('/health-check')
+def check():
+    """Health-check endpoint"""
+    return jsonify(
+    status=200
+)
+
+@app.route('/mail')
+def send_mail():
+    """Send detailed result from form by mail endpoint"""
+    msg = Message(subject='Questionnaire mouvement en entreprise', sender='elouandacostapeixoto@gmail.com', recipients=['elouan@bodih.fr'])
+    msg.body = "Résultat détaillé du questionnaire mouvement en entreprise de Bodih."
+    mail.send(msg)
+    return "Message sent!"
 
 
-    @app.route('/health-check')
-    def check():
-        return jsonify(
-        status=200
-    )
-    return app
+if __name__ == '__main__':
+    app.run(debug=True)
