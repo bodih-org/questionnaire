11e165095709dfaf56f50d0a1139114430798764
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2025-03-05 12:46:37 +0100

    feat: update ResultsChart component to display dynamic results data

--- front/src/components/ResultsChart.tsx
+++ front/src/components/ResultsChart.tsx
@@ -24,21 +24,26 @@ ChartJS.register(
   Legend
 );
 
-export const data = {
-  labels: ['Thing 1', 'Thing 2', 'Thing 3', 'Thing 4', 'Thing 5', 'Thing 6'],
-  datasets: [
-    {
-      label: '# of Votes',
-      data: [2, 9, 3, 5, 2, 3],
-      backgroundColor: 'rgba(255, 99, 132, 0.2)',
-      borderColor: 'rgba(255, 99, 132, 1)',
-      borderWidth: 1,
-    },
-  ],
-};
 
 
-export const ResultsChart: React.FC<ResultsChartProps> = ({ results }) =>{
+export const ResultsChart: React.FC<ResultsChartProps> = ({ results }) => {
+  const resultData = [
+    results.physical,
+    results.cognitive,
+    results.organizational
+  ]
+  const data = {
+    labels: ['Physique', 'Cognitif', 'Organisationnel'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: resultData,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
   return (
     <div className="bg-white p-8 rounded-lg shadow-lg">
       <h2 className="text-2xl font-bold mb-6 text-center">Résultats de l'évaluation</h2>
